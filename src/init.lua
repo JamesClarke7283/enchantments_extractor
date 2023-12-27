c0d33603d14786357ac5ad528a754b90097fffa9
James David Clarke <james@jamesdavidclarke.com>
2023-12-27 07:55:54 +0000

    Use table.concat for formspec

--- src/init.lua
+++ src/init.lua
@@ -3,60 +3,37 @@ local function get_extractor_formspec()
     local formspec = {
         "formspec_version[4]",
         "size[11.75,10.425]",
-    }
-
-    -- Helper function to add inventory components to the formspec
-    local function add_inventory_components(label, bg, list, listring)
-        table.insert(formspec, "label" .. label)
-        table.insert(formspec, bg)  -- Add the background right after the label
-        table.insert(formspec, "list" .. list)
-        if listring then
-            table.insert(formspec, "listring" .. listring)
-        end
-    end
-
-    -- Define labels, backgrounds, lists, and listrings for the UI elements with consistent positioning
-    local labels = {
-        "[0.375,0.375;" .. minetest.formspec_escape("Enchantment Extractor") .. "]",
-        "[0.375,1;" .. minetest.formspec_escape("Input Book:") .. "]",
-        "[2.375,1;" .. minetest.formspec_escape("Enchanted Item:") .. "]",
-        "[0.375,2.5;" .. minetest.formspec_escape("Output Books:") .. "]",
-        "[0.375,4.7;" .. minetest.formspec_escape("Inventory") .. "]",
-    }
-
-    local bgs = {
+        -- Labels
+        "label[0.375,0.375;", minetest.formspec_escape("Enchantment Extractor"), "]",
+        "label[0.375,1;", minetest.formspec_escape("Input Book:"), "]",
+        "label[2.375,1;", minetest.formspec_escape("Enchanted Item:"), "]",
+        "label[0.375,2.5;", minetest.formspec_escape("Output Books:"), "]",
+        "label[0.375,4.7;", minetest.formspec_escape("Inventory"), "]",
+        -- Slot backgrounds
         mcl_formspec.get_itemslot_bg_v4(0.375, 0.75, 1, 1),
         mcl_formspec.get_itemslot_bg_v4(2.375, 0.75, 1, 1),
         mcl_formspec.get_itemslot_bg_v4(0.375, 3, 8, 1),
         mcl_formspec.get_itemslot_bg_v4(0.375, 5.1, 9, 3),
         mcl_formspec.get_itemslot_bg_v4(0.375, 9.05, 9, 1),
+        -- Slot lists
+        "list[context;input_book;0.375,0.75;1,1;]",
+        "list[context;input_enchanted;2.375,0.75;1,1;]",
+        "list[context;output_books;0.375,3;8,1;]",
+        "list[current_player;main;0.375,5.1;9,3;9]",
+        "list[current_player;main;0.375,9.05;9,1;]",
+        -- Listrings
+        "listring[context;output_books]",
+        "listring[current_player;main]",
+        "listring[context;input_book]",
+        "listring[current_player;main]",
+        "listring[context;input_enchanted]",
+        "listring[current_player;main]",
     }
-
-    local lists = {
-        "[context;input_book;0.375,0.75;1,1;]",
-        "[context;input_enchanted;2.375,0.75;1,1;]",
-        "[context;output_books;0.375,3;8,1;]",
-        "[current_player;main;0.375,5.1;9,3;9]",
-        "[current_player;main;0.375,9.05;9,1;]",
-    }
-
-    local listrings = {
-        "[context;output_books]",
-        "[current_player;main]",
-        "[context;input_book]",
-        "[current_player;main]",
-        "[context;input_enchanted]",
-        "[current_player;main]",
-    }
-
-    -- Add each component group to the formspec
-    for i = 1, #labels do
-        add_inventory_components(labels[i], bgs[i], lists[i], listrings[i])
-    end
 
     -- Return the concatenated formspec string
     return table.concat(formspec)
 end
+
 
 
 
@@ -100,6 +77,7 @@ minetest.register_node("enchantments_extractor:enchantment_extractor", {
         return 0 -- Disallow by default for any other lists
     end,
     on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+        minetest.log("action", clicker:get_player_name() .. " formspec: " .. get_extractor_formspec())
         minetest.show_formspec(clicker:get_player_name(), "enchantments_extractor:enchantment_extractor_form", get_extractor_formspec())
     end,
     _mcl_blast_resistance = 1200,
