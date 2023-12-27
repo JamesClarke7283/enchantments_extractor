d7b1abd9a2a9561c644f68cc97ec280f46b3c6ce
James David Clarke <james@jamesdavidclarke.com>
2023-12-27 08:26:15 +0000

    Made top slots interactive and put empty book texture in correct slots

--- src/init.lua
+++ src/init.lua
@@ -2,26 +2,34 @@ local function get_extractor_formspec()
     -- Define the formspec version and size
     local formspec = {
         "formspec_version[4]",
-        "size[11.75,10.425]",
+        "size[11.75,11.425]",
+        
         -- Labels
         "label[0.375,0.375;", minetest.formspec_escape("Enchantment Extractor"), "]",
-        "label[0.375,1;", minetest.formspec_escape("Input Book:"), "]",
-        "label[2.375,1;", minetest.formspec_escape("Enchanted Item:"), "]",
-        "label[0.375,2.5;", minetest.formspec_escape("Output Books:"), "]",
-        "label[0.375,4.7;", minetest.formspec_escape("Inventory"), "]",
-        -- Slot backgrounds
-        mcl_formspec.get_itemslot_bg_v4(0.375, 0.75, 1, 1),
-        mcl_formspec.get_itemslot_bg_v4(2.375, 0.75, 1, 1),
-        mcl_formspec.get_itemslot_bg_v4(0.375, 3, 8, 1),
-        mcl_formspec.get_itemslot_bg_v4(0.375, 5.1, 9, 3),
-        mcl_formspec.get_itemslot_bg_v4(0.375, 9.05, 9, 1),
-        -- Slot lists
-        "list[context;input_book;0.375,0.75;1,1;]",
-        "list[context;input_enchanted;2.375,0.75;1,1;]",
-        "list[context;output_books;0.375,3;8,1;]",
-        "list[current_player;main;0.375,5.1;9,3;9]",
-        "list[current_player;main;0.375,9.05;9,1;]",
-        -- Listrings
+        "label[0.875,1.25;", minetest.formspec_escape("Input Book:"), "]",
+        "label[3.875,1.25;", minetest.formspec_escape("Enchanted Item:"), "]",
+        "label[0.375,3.75;", minetest.formspec_escape("Output Books:"), "]",
+        "label[0.375,5.95;", minetest.formspec_escape("Inventory"), "]",
+        
+        -- Slot backgrounds for the node's inventory
+        mcl_formspec.get_itemslot_bg_v4(0.875, 1.5, 1, 1, 0, "mcl_formspec_itemslot.png^mcl_book_book_empty_slot.png"),
+        mcl_formspec.get_itemslot_bg_v4(3.875, 1.5, 1, 1),
+        mcl_formspec.get_itemslot_bg_v4(0.375, 4, 8, 1, 0, "mcl_formspec_itemslot.png^mcl_book_book_empty_slot.png"),
+        
+        -- Slot lists for the node's inventory
+        "list[context;input_book;0.875,1.5;1,1;]",
+        "list[context;input_enchanted;3.875,1.5;1,1;]",
+        "list[context;output_books;0.375,4;8,1;]",
+        
+        -- Slot backgrounds for the player's inventory
+        mcl_formspec.get_itemslot_bg_v4(0.375, 6.2, 9, 3),
+        mcl_formspec.get_itemslot_bg_v4(0.375, 10.15, 9, 1),
+        
+        -- Slot lists for the player's inventory
+        "list[current_player;main;0.375,6.2;9,3;9]",
+        "list[current_player;main;0.375,10.15;9,1;]",
+        
+        -- Listrings to allow moving items between lists
         "listring[context;output_books]",
         "listring[current_player;main]",
         "listring[context;input_book]",
@@ -33,7 +41,6 @@ local function get_extractor_formspec()
     -- Return the concatenated formspec string
     return table.concat(formspec)
 end
-
 
 
 
@@ -55,6 +62,8 @@ minetest.register_node("enchantments_extractor:enchantment_extractor", {
         inv:set_size("input_book", 1)
         inv:set_size("input_enchanted", 1)
         inv:set_size("output_books", 9)
+        local form = get_extractor_formspec()
+        meta:set_string("formspec", form)
     end,
     allow_metadata_inventory_put = function(pos, listname, index, stack, player)
         if listname == "input_book" then
@@ -75,10 +84,6 @@ minetest.register_node("enchantments_extractor:enchantment_extractor", {
             return 0 -- Disallow placing items manually in output
         end
         return 0 -- Disallow by default for any other lists
-    end,
-    on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
-        minetest.log("action", clicker:get_player_name() .. " formspec: " .. get_extractor_formspec())
-        minetest.show_formspec(clicker:get_player_name(), "enchantments_extractor:enchantment_extractor_form", get_extractor_formspec())
     end,
     _mcl_blast_resistance = 1200,
     _mcl_hardness = 5
